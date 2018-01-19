acc2e810fd50114f80d88fa37c5d5f9aa4abebc5
jaymccormick <jaymccormi@gmail.com>
2018-01-19 14:47:53 -0500

    svg circles with calculated attributes

--- script.js
+++ script.js
@@ -2,9 +2,29 @@
 var dataset = [];
 
 // make dataset list of random numbers
-for (var i = 0; i < 25; i++){
-  dataset.push(Math.round(Math.random() * 30 + 3));
+for (var i = 0; i < 10; i++){
+  dataset.push(Math.round(Math.random() * 20 + 1));
 }
+
+// let's work with svg
+// width and height variables
+var w = 500;
+var h = 50;
+// add the svg element that will hold other svg elements
+var svg = d3.select("body")
+            .append("svg")
+            .attr("width", w)
+            .attr("height", h);
+
+var circles = svg.selectAll("circle")
+    .data(dataset)
+    .enter()
+    .append("circle")
+
+circles.attr("cx", (d, i) => i * 50 + 25)
+        .attr("cy", h / 2)
+        .attr("r", d => d);
+
 
 // svg elements
 // circle <circle cx="250" cy="150" r="50"/>
