46c45f35177243a70edd72174afaa52f20fb1ab3
jaymccormick <jaymccormi@gmail.com>
2018-01-19 08:48:27 -0500

    data, enter(), d => d

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
+
+
+/**
+
+// data, enter and d function
+var dataset = [3, 6, 9, 12, 15];
+
+d3.select("body").selectAll("p")
+  .data(dataset)
+  .enter()
+  .append("p")
+  .text(d => d);
+
+
+// simple append
 d3.select("body").append("p")
   .text("new paragraph!")
+*/
