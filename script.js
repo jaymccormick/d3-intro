4f8e032eceb40a95270ce5b0e7b493b716200049
jaymccormick <jaymccormi@gmail.com>
2018-01-19 08:59:38 -0500

    set text style with d => d

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+var dataset = [3, 6, 9, 12, 15];
 
 
 /**
 
-// data, enter and d function
-var dataset = [3, 6, 9, 12, 15];
+// setting css text color with d
+d3.select("body").selectAll("p")
+  .data(dataset)
+  .enter()
+  .append("p")
+  .text(d => d)
+  .style("color", d => "rgb(0," + (d * 10 - 30) + "," + (d * 15 + 30) + ")");
 
+
+
+// data, enter and d function
 d3.select("body").selectAll("p")
   .data(dataset)
   .enter()
